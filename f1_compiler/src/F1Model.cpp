496fd1fa3bb2540f1274f2cfcbc0118a33aae4b7
Eric Tai <hytai.yan@gmail.com>
2026-05-03 16:29:08 +0800

    feat(F1): Add missing cmake command

--- f1_compiler/src/F1Model.cpp
+++ f1_compiler/src/F1Model.cpp
@@ -10,7 +10,7 @@
 // For older versions like GCC 11, we fall back to the {fmt} library and
 // provide a compatibility alias in the std namespace.
 #if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 13
-#include <fmt/format.h>
+#include <fmt/core.h>
 namespace std { using fmt::format; }
 #else
 #include <format>
